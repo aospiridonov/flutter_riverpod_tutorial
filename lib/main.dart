a71b85a0c9dbae0de15163f908c82b21dc6d87ae
aospiridonov <aospiridonov@gmail.com>
2022-01-20 14:27:10 +0300

    add FutureProvider

--- lib/main.dart
+++ lib/main.dart
@@ -2,35 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() => runApp(
-      const ProviderScope(
+      ProviderScope(
         child: MyApp(),
       ),
     );
 
-final firstStringProvider = Provider((ref) => 'First');
-final secondStringProvider = Provider((ref) => 'Second');
+class FakeHttpClient {
+  Future<String> get(String url) async {
+    await Future.delayed(const Duration(seconds: 1));
+    return 'Responce from $url';
+  }
+}
+
+final fakeHttpClientProvider = Provider((ref) => FakeHttpClient());
+final responceProvider =
+    FutureProvider.family<String, String>((ref, url) async {
+  final httpClient = ref.read(fakeHttpClientProvider);
+  return httpClient.get(url);
+});
 
 class MyApp extends ConsumerWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final first = ref.watch(firstStringProvider);
-    final second = ref.watch(secondStringProvider);
     return MaterialApp(
       title: 'Riverpod Tutorial',
       home: Scaffold(
-        body: Column(
-          children: [
-            Text(
-              first,
-              style: TextStyle(fontSize: 30),
-            ),
-            Text(
-              second,
-              style: TextStyle(fontSize: 30),
-            ),
-          ],
+        body: Center(
+          child: Consumer(
+            builder: (context, ref, child) {
+              final responseAsyncValue =
+                  ref.watch(responceProvider('https://youtube.com'));
+              return responseAsyncValue.map(
+                data: (_) => Text(_.value),
+                error: (_) => Text(
+                  _.error.toString(),
+                  style: TextStyle(
+                    color: Colors.red,
+                  ),
+                ),
+                loading: (_) => CircularProgressIndicator(),
+              );
+            },
+          ),
         ),
       ),
     );
