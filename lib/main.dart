957ef038cdadceda4c7a9677cc094d5cf6f83c73
aospiridonov <aospiridonov@gmail.com>
2022-01-20 13:12:43 +0300

    increment provider from use ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,49 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(
+      const ProviderScope(
+        child: MyApp(),
+      ),
+    );
+
+class IncrementNotifier extends ChangeNotifier {
+  int _value = 0;
+  int get value => _value;
+
+  void increment() {
+    _value++;
+    notifyListeners();
+  }
 }
 
-class MyApp extends StatelessWidget {
+final incrementProvider = ChangeNotifierProvider((ref) => IncrementNotifier());
+
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Riverpod Tutorial',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Riverpod Tutorial'),
         ),
-        body: const MyWidget(),
-      ),
-    );
-  }
-}
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Provider(
-      create: (context) => 'A String far away.',
-      child: Provider(
-        create: (context) => 'A String that is close.',
-        builder: (context, child) {
-          return Text(Provider.of<String>(context));
-        },
+        body: Center(
+          child: Consumer(
+            builder: (context, ref, child) {
+              final incrementNotifier = ref.watch(incrementProvider);
+              return Text(incrementNotifier.value.toString());
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(incrementProvider).increment();
+          },
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
